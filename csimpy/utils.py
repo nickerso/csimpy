9bb319ced0097cb7e21004e0fdab85b6682595ab
David Nickerson <david.nickerson@gmail.com>
2020-05-08 23:43:34 +1200

    Updating to correctly pull out all namespaces for the XPath context node.
    Using the fix from https://github.com/fbergmann/libSEDML/issues/77 to pull out the
    namespaces for the current node and all parents. Adding test local namespace
    declarations in the test SED-ML file to make sure this works.

--- csimpy/utils.py
+++ csimpy/utils.py
@@ -4,18 +4,26 @@ import libsedml
 def get_xpath_namespaces(sed_element):
     """
     Get all the XML namespaces for the given SED-ML element. See https://github.com/fbergmann/libSEDML/issues/77 for
-    an issue on getting the actual namespaces, but for now looks like this will only return namespaces declared on
-    the root element of the SED-ML document.
+    some background. This will collect all namespaces from the current element and all its parents.
 
     :param sed_element: the SED-ML element to define the context for fetching namespaces.
     :return: a dictionary of defined namespaces.
     """
     ns = {}
-    xmlns = sed_element.getNamespaces()
-    for n in range(0, xmlns.getNumNamespaces()):
-        prefix = xmlns.getPrefix(n)
-        # we can't use a default namespace in XPath so filter it out...
-        if prefix != '':
-            uri = xmlns.getURI(n)
-            ns[prefix] = uri
+    current_sed_base = sed_element
+    while current_sed_base:
+        xmlns = current_sed_base.getElementNamespaces()
+        if xmlns:
+            for n in range(0, xmlns.getNumNamespaces()):
+                prefix = xmlns.getPrefix(n)
+                if prefix == '':
+                    # we can't use a default namespace in XPath so filter it out...
+                    continue
+                elif prefix in ns:
+                    # existing namespace prefixes should not be changed
+                    continue
+                else:
+                    uri = xmlns.getURI(n)
+                    ns[prefix] = uri
+        current_sed_base = current_sed_base.getParentSedObject()
     return ns
