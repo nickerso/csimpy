0ae5ae11b1f0f67e2b50e3931953fdd3da00a644
David Nickerson <david.nickerson@gmail.com>
2022-01-17 17:54:54 +1300

    tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from os import path
+import re
 from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+def get_version(this_directory):
+    with open(path.join(this_directory, 'csimpy/_version.py'), encoding='utf-8') as f:
+        version_file = f.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                  version_file, re.M)
+        if version_match:
+            return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
-    name='CSimPy',
-    version='0.0.2a',
+    name='csimpy',
+    version=get_version(this_directory),
     py_modules=['csimpy'],
-    package_dir={'': 'csimpy'},
+    package_dir={'': '.'},
     url='https://github.com/nickerso/csimpy',
     license='Apache 2.0',
     author='David Nickerson',
